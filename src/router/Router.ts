38152bfc159f9c10e3ae3656a5c9cec91443bee3
Chloekkk <skdusdl8804@gmail.com>
2022-09-04 15:04:01 +0900

    feat: Add Router class

---
+++ src/router/Router.ts
@@ -0,0 +1,73 @@
+import IComponent from '../interface/IComponent.js';
+import ChatPage from '../pages/ChatPage.js';
+
+export default class Router {
+  #routes: Route[];
+  constructor() {
+    this.#routes = [];
+  }
+  addRoute(route: Route) {
+    this.#routes.push(route);
+  }
+  getRoutes() {
+    this.#routes.forEach((route: Route) => {
+      console.log(route);
+    });
+  }
+  static pathToRegex(path: string): RegExp {
+    // ^ : start, path.replace(/\//g, "\\/"): / to \\/ ??? unknown result....
+    // replace(/:\w+/g, '(.+)'): , $: end
+    return new RegExp(
+      '^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$'
+    );
+  }
+  static getParams = (match: {route: Route; result: string[]}) => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.getPath().matchAll(/:(\w+)/g)).map(
+      result => result[1]
+    );
+    const params: string[][] = [];
+    keys.forEach((key, i) => {
+      params.push([key, values[i]]);
+    });
+    return params;
+  };
+  render() {
+    const matchRoutes = this.#routes.map(route => {
+      return {
+        route: route,
+        result: location.pathname.match(Router.pathToRegex(route.getPath())),
+      };
+    });
+  }
+}
+
+export class Route {
+  #path: string;
+  #component: typeof IComponent;
+  constructor(path: string, component: typeof IComponent) {
+    this.#path = path;
+    this.#component = component;
+  }
+  async getComponent() {
+    return this.#component;
+  }
+  getPath() {
+    return this.#path;
+  }
+}
+
+class Home extends IComponent {
+  constructor(params: Object) {
+    super(params);
+  }
+  async getHTML(): Promise<string> {
+    return '<h1>HOME, Hello World</h1>';
+  }
+}
+
+const BrowserRouter = new Router();
+BrowserRouter.addRoute(new Route('/', Home));
+BrowserRouter.addRoute(new Route('/chat', ChatPage));
+BrowserRouter.addRoute(new Route('/chat:id', ChatPage));
+BrowserRouter.getRoutes();
